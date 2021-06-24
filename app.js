5a89f836e470ebfa4fbb817c47170aa9960bf18f
chamindujs <chamindujs@gmail.com>
2021-06-24 14:54:50 +0530

    Delete JS

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-const title = document.querySelector('.details')
-
-function getTitle(doc) {
-    let li = document.createElement('li')
-    let name = document.createElement('span')
-}
-
-
-db.collection('jobs').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        getTitle(doc)
-    })
-})
-
-
 
 
